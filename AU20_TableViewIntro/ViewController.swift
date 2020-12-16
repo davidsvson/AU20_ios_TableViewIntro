8bf4a49a6f7149c08cbf1d6e25d12c770fc31454
David Svensson <davidsvson@gmail.com>
2020-12-16 09:09:54 +0100

    Initial Commit

---
+++ AU20_TableViewIntro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AU20_TableViewIntro
+//
+//  Created by David Svensson on 2020-12-16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
